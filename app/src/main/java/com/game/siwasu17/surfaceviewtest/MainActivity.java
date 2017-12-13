a4eaa0ec70f077d9b5d80ce2cf810fef4a0a19a2
siwasu17 <siwasu17@gmail.com>
2017-12-14 01:11:35 +0900

    Add AlertDialog.

--- app/src/main/java/com/game/siwasu17/surfaceviewtest/MainActivity.java
+++ app/src/main/java/com/game/siwasu17/surfaceviewtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.game.siwasu17.surfaceviewtest;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -14,6 +16,8 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -42,9 +46,6 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         Picasso.with(this)
                 .load(R.drawable.iiwake)
                 .into(imageView);
-
-        this.getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 
     }
 
@@ -113,6 +114,43 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            final EditText editView = new EditText(MainActivity.this);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setView(editView);
+            // OKボタンの設定
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String t = editView.getText().toString();
+                    // OKボタンをタップした時の処理をここに記述
+                    System.out.println("Enter: " + t);
+                    MainActivity.this.text = t;
+                }
+            });
+
+            // キャンセルボタンの設定
+            builder.setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // キャンセルボタンをタップした時の処理をここに記述
+                }
+            });
+
+
+            AlertDialog dialog = builder.create();
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialogInterface) {
+                    InputMethodManager inputMethodManager
+                            = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.showSoftInput(editView, 0);
+                }
+            });
+            dialog.show();
+
+
+        }
+
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             x = (int) event.getX();
             y = (int) event.getY();
@@ -124,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
      * 描画系
      */
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private String text = ">";
+    private String text = "";
     private int x = 0;
     private int y = 0;
 
